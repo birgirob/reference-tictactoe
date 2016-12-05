90a1e41586c188e0279287fd183a5484157a3d62
Birgir <birgirob@gmail.com>
2016-12-05 14:06:25 +0000

    Change the migrations script to take in Node environment parameters

--- migrations.sh
+++ migrations.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+# Get node environment (production/development) from first argument
+node_env=$1
+# Shift the arguments by 1 to execute the rest later
+shift 1
+
+echo Node environment: $node_env
+
+# Sleep to make sure the database is up
+# And execute the database migrations
 sleep 5
-npm run migratedb
+npm run migratedb:$node_env
+
+# Execute the rest of the arguments
 exec "$@"
