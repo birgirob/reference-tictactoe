806f65994a16ca54c5912fa1fe65efd1e741261c
Birgir <birgirob@gmail.com>
2016-12-09 13:38:47 +0000

    Added jasmine XML test reports

--- server/jasmine-setup.js
+++ server/jasmine-setup.js
@@ -2,6 +2,8 @@ require('./globals');
 
 
 var JasmineConsoleReporter = require('jasmine-console-reporter');
+var reporters = require('jasmine-reporters');
+
 var consoleReporter = new JasmineConsoleReporter({
     colors: 1,           // (0|false)|(1|true)|2
     cleanStack: 1,       // (0|false)|(1|true)|2|3
@@ -9,4 +11,11 @@ var consoleReporter = new JasmineConsoleReporter({
     listStyle: 'indent', // "flat"|"indent"
     activity: false
 });
+
+var junitReporter = new reporters.JUnitXmlReporter({
+    savePath: './test-results',
+    consolidateAll: false
+});
+
 jasmine.getEnv().addReporter(consoleReporter);
+jasmine.getEnv().addReporter(junitReporter);
